2cd1fe35ecefbe22c5926897393b7b13ffc63c59
g.ibarraa <g.ibarraas@gmail.com>
2025-10-27 19:02:28 -0600

    se agrego el search by id y se agregaron filtros al get/concerts

--- src/routes/concerts.routes.js
+++ src/routes/concerts.routes.js
@@ -2,4 +2,5 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/concerts.controller");
 router.get("/", controller.list);
+router.get("/:id", controller.detail);
 module.exports = router;
